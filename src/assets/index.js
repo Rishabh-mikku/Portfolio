067f67ed7ba4cc1d349c0c0e9a8bd51646ed51a8
Rishabh-mikku <mikkurishabh94@gmail.com>
2024-06-08 17:27:02 +0530

    20240608 Extension of project 3 changed from jpeg to jpg

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import logo from "./images/logo.png";
 import bannerImg from "./images/bannerImg.png";
 import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.jpg";
-import projectThree from "./images/projects/projectThree.jpeg";
+import projectThree from "./images/projects/projectThree.jpg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import quote from "./images/testmonial/quote.png";
